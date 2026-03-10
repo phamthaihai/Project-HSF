03dcfc636ee48a5cf27644c1f6f1ceec80ebcfd5
LongNV2004 <longnguyen01042004@gmail.com>
2026-03-10 14:48:02 +0700

    fixuserrepo

--- src/main/java/swt/he182176/hsfproject/repository/UserRepository.java
+++ src/main/java/swt/he182176/hsfproject/repository/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import swt.he182176.hsfproject.entity.User;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -16,4 +17,6 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     boolean existsByEmailAndIdNot(String email, Integer id);
 
     Optional<User> findByVerifyToken(String verifyToken);
+
+    List<User> findByRole_Name(String roleName);
 }
